3ba5e48d8d095bfa81d0f303ca11084275020947
CppPhil <CppPhil@users.noreply.github.com>
2020-03-26 00:34:05 +0100

    Stupid Poco won't create the repo

--- files/Chapter04/cpp/exercise1/hello.cpp
+++ files/Chapter04/cpp/exercise1/hello.cpp
@@ -1,12 +1,24 @@
+#include <cstdio>
+
+#include <Poco/Data/MySQL/MySQLException.h>
+
 #include "hello.hpp"
 #include "people/repository.hpp"
 
 namespace e1 {
 void entry_point() {
-  const people::repository repo;
+  try {
+    fprintf(stderr, "About to create repo\n");
+    const people::repository repo;
+    fprintf(stderr, "Created repo\n");
 
-  const model::person result_value(repo.get_person("Gru"));
+    fprintf(stderr, "About to call get_person\n");
+    const model::person result_value(repo.get_person("Gru"));
+    fprintf(stderr, "Finished calling get_person\n");
 
+  } catch (const Poco::Data::MySQL::MySQLException& ex) {
+    fprintf(stderr, "Caught MySQLException: %s\n", ex.what());
+  }
   // TODO: Implement HTTP server
 }
 } // namespace e1
