bd53f184c1796030ad0bd2470744857167921d84
CppPhil <CppPhil@users.noreply.github.com>
2020-03-30 16:50:36 +0200

    Python DB doesn't work; what a trash language

--- Chapter04/python/exercise1/hello.py
+++ Chapter04/python/exercise1/hello.py
@@ -1,34 +1,26 @@
 from flask import Flask
 from .database import Person
-from lib.tracing import init_tracer
-import opentracing
 
-app = Flask('py-2-hello')
-init_tracer('py-2-hello')
+
+app = Flask('py-1-hello')
+
 
 @app.route("/sayHello/<name>")
 def say_hello(name):
-    with opentracing.tracer.start_span('say-hello') as span:
-        person = get_person(name, span)
-        resp = format_greeting(
-            name=person.name,
-            title=person.title,
-            description=person.description,
-        )
-        span.set_tag('response', resp)
-        return resp
+    person = get_person(name)
+    resp = format_greeting(
+        name=person.name,
+        title=person.title,
+        description=person.description,
+    )
+    return resp
 
 
-def get_person(name, span):
+def get_person(name):
     person = Person.get(name)
     if person is None:
         person = Person()
         person.name = name
-    span.log_kv({
-        'name': person.name,
-        'title': person.title,
-        'description': person.description
-    })
     return person
 
 
