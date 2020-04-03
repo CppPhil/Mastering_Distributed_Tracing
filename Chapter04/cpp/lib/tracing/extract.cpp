4c92c833ea0bd1515b82ae3a7d0daf20f853469b
CppPhil <CppPhil@users.noreply.github.com>
2020-04-03 06:06:04 +0200

    Convert exceptions from pl::unhexify to tl::expected

--- Chapter04/cpp/lib/tracing/extract.cpp
+++ Chapter04/cpp/lib/tracing/extract.cpp
@@ -18,21 +18,27 @@ namespace {
 template <class T>
 tl::expected<std::unique_ptr<opentracing::SpanContext>, util::error>
 extract_impl(const T& x) {
-  const std::string& header_value_hex_string
-    = x.getHeader("OPENTRACING_SPAN_CONTEXT");
+  try {
+    const std::string& header_value_hex_string
+      = x.getHeader("OPENTRACING_SPAN_CONTEXT");
 
-  const std::vector<pl::byte> data(pl::unhexify(header_value_hex_string, 0));
+    const std::vector<pl::byte> data(pl::unhexify(header_value_hex_string, 0));
 
-  opentracing::expected<std::unique_ptr<opentracing::SpanContext>> exp(
-    extract(data.data(), data.size()));
+    opentracing::expected<std::unique_ptr<opentracing::SpanContext>> exp(
+      extract(data.data(), data.size()));
 
-  if (!exp.has_value()) {
-    std::ostringstream oss;
-    oss << exp.error();
-    return UTIL_UNEXPECTED(oss.str());
+    if (!exp.has_value()) {
+      std::ostringstream oss;
+      oss << exp.error();
+      return UTIL_UNEXPECTED(oss.str());
+    }
+
+    return *std::move(exp);
+  } catch (const std::exception& ex) {
+    return UTIL_UNEXPECTED(ex.what());
+  } catch (...) {
+    return UTIL_UNEXPECTED("Unknown exception caught!");
   }
-
-  return *std::move(exp);
 }
 } // namespace
 
