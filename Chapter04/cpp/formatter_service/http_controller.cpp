ec0a016f841380622658120045d0e743399cf5ea
CppPhil <CppPhil@users.noreply.github.com>
2020-04-03 05:33:43 +0200

    Do formatter_service

--- Chapter04/cpp/formatter_service/http_controller.cpp
+++ Chapter04/cpp/formatter_service/http_controller.cpp
@@ -1,5 +1,6 @@
 #include "http_controller.hpp"
 #include "format_greeting.hpp"
+#include "tracing/extract.hpp"
 
 namespace e4 {
 http_controller::http_controller() {
@@ -9,7 +10,15 @@ void http_controller::handle_format_greeting(
   const drogon::HttpRequestPtr& req,
   std::function<void(const drogon::HttpResponsePtr&)>&& callback,
   model::person&& person) const {
-  auto span = opentracing::Tracer::Global()->StartSpan("/formatGreeting");
+  auto ctx = tracing::extract(*req);
+  auto span = (!ctx.has_value() || *ctx == nullptr)
+    ? opentracing::Tracer::Global()
+        ->StartSpan("/formatGreeting") opentracing::Tracer::Global()
+        ->StartSpan("/formatGreeting", {opentracing::ChildOf(cxt->get())});
+
+  // TODO: See if this looks like Go in Jaeger
+  span->SetTag("span.kind", "server");
+
   auto resp = drogon::HttpResponse::newHttpResponse();
 
   const auto greeting = format_greeting(person.name(), person.title(),
